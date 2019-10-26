76589441eed7d7d57258794fe7553355e6ce067c
Dmitriy Shabat <mitya.shabat@gmail.com>
2019-10-26 11:25:37 +0300

    Added the same thingie for real estate

---
+++ real_estate_parser.py
@@ -0,0 +1,35 @@
+import requests
+
+API = 'https://declarator.org/api/v1/search/sections'
+
+# Usage example
+# RealEstateParser.request_real_estate(region=64) returns list of
+# {square:xxx, year:xxx, person: {id:xxx, name:xxx}}
+class RealEstateParser:
+    @staticmethod
+    def simplify_section(section):
+        square = 0
+        for est in section['real_estates']:
+            square += est['square']
+        return {
+            'square': square,
+            'year': section['main']['year'],
+            'person': {
+                'id': section['main']['person']['id'],
+                'name': section['main']['person']['name']
+            }
+        }
+    @staticmethod
+    def request_real_estate(pages = None, **params):
+        res = requests.get(API, params).json()
+        sections = []
+        i = 1
+        while res['next'] is not None:
+            print('Getting page {}'.format(i))
+            sections += list(map(RealEstateParser.simplify_section, res['results']))
+            i += 1
+            if pages is not None and i > pages:
+                break
+            res = requests.get(res['next']).json()
+        return sections
+        
