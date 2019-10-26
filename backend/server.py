83544be3112e1d6cb775c3b0dcfe56d3c191c255
hi_melnikov <melnikovam10@gmail.com>
2019-10-26 21:55:00 +0300

    some commenting was done

--- backend/server.py
+++ backend/server.py
@@ -13,9 +13,14 @@ def result(office_id):
     print("CALL", office_id)
     family = 0
     office = office_data(office_id, family)
-    print(office.true_avg("incomes"))
-    print(office.gender_avg("incomes"))
-    print(office.party_avg("incomes"))
+    print(office.true_avg("incomes")) # просто средняя зарплата
+    # Пример: [975657.0, 1063445.0, 1215654.67, 1397083.65, 1769658.0, 2213239.15, 3031778.9, 2769537.14, 1567386.81, 1478636.39, 1445398.08]
+    print(office.gender_avg("incomes")) # по полу средняя зарплата
+    # Пример: [975657.0, 1063445.0, 1215654.67, 1397083.65, 1769658.0, 2213239.15, 3031778.9, 2769537.14, 1567386.81, 1478636.39, 1445398.08], [None, None, None, None, None, None, None, None, None, None, None]
+    print(office.party_avg("incomes")) # по партиям средняя зарплата
+    # Пример: (['Нет Данных', 'Единая Россия'], [[None, None, None, None, None, None, None, 2769537.14, 1567386.81, 1478636.39, 1445398.08], [975657.0, 1063445.0, 1215654.67, 1397083.65, 1769658.0, 2213239.15, 3031778.9, None, None, None, None]])
+
+    # Денис скоро добавит остальные функции 
     return render_template("result.html")
 
 if __name__ == "__main__":
