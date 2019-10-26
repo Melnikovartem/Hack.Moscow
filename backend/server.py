adb345aecf5acf0dd75fefbc994cf5e697b6d2a5
Your Name <you@example.com>
2019-10-27 02:27:10 +0300

    графики зарплаты

--- backend/server.py
+++ backend/server.py
@@ -16,16 +16,38 @@ def result():
     family = request.args.get('family', default=0, type=int)
 
     office = office_data(office_id)
-    avarage_salary = office.true_avg("incomes")  # просто средняя зарплата
+    avarage_salary = office.true_avg("incomes")
+    years = list(office.data)  # просто средняя зарплата
     # Пример: [975657.0, 1063445.0, 1215654.67, 1397083.65, 1769658.0, 2213239.15, 3031778.9, 2769537.14, 1567386.81, 1478636.39, 1445398.08]
-    print(office.gender_avg("incomes"))# по полу средняя зарплата
+    m_avarage_salary = office.gender_avg("incomes")[0]
+    w_avarage_salary = office.gender_avg("incomes")[1]
+    # по полу средняя зарплата
     # Пример: [975657.0, 1063445.0, 1215654.67, 1397083.65, 1769658.0, 2213239.15, 3031778.9, 2769537.14, 1567386.81, 1478636.39, 1445398.08], [None, None, None, None, None, None, None, None, None, None, None]
-    print(office.party_avg("incomes"))  # по партиям средняя зарплата
+    part_names = office.party_avg("incomes")[0]
+    part_salaries = office.party_avg(
+        "incomes")[1]  # по партиям средняя зарплата
     # Пример: (['Нет Данных', 'Единая Россия'], [[None, None, None, None, None, None, None, 2769537.14, 1567386.81, 1478636.39, 1445398.08], [975657.0, 1063445.0, 1215654.67, 1397083.65, 1769658.0, 2213239.15, 3031778.9, None, None, None, None]])
 
     # Денис скоро добавит остальные функции к
+    for s in range(len(w_avarage_salary)):
+        if w_avarage_salary[s] == None:
+            w_avarage_salary[s] = 'null'
 
-    return render_template("result.html", family=family, avarage_salary=avarage_salary)
+    for s in range(len(m_avarage_salary)):
+        if m_avarage_salary[s] == None:
+            m_avarage_salary[s] = 'null'
+    for s in range(len(avarage_salary)):
+        if avarage_salary[s] == None:
+            avarage_salary[s] = 'null'
+
+    for salary in part_salaries:
+
+        for s in range(len(salary)):
+            if salary[s] == None:
+                salary[s] = 'null'
+
+    return render_template("result.html", family=family, avarage_salary=avarage_salary, years=years, m_avarage_salary=m_avarage_salary, w_avarage_salary=w_avarage_salary, part_names=part_names, part_salaries=part_salaries)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ backend/static/js/main.js
@@ -0,0 +1,478 @@
+$('#open-mobile-menu').click(function() {
+    $('.mobile-menu').slideToggle()
+    $('#close-mobile-menu').toggle()
+})
+
+$('#close-mobile-menu').click(function() {
+    $('#close-mobile-menu').toggle()
+    $('.mobile-menu').slideToggle()
+})
+
+$('.filters button').click(function() {
+    $('.active').removeClass('active')
+    $(this).addClass('active')
+})
+
+
+//graphics
+
+//!salary
+function salary(type_e, data_e, labels_e, data_man, data_woman, from, to, parts) {
+    var labels_o = []
+    data_o = []
+    data_man_o = []
+    data_woman_o = []
+
+    for (var i = 0; i <= labels_e.length; i++) {
+        if (labels_e[i] <= to & labels_e[i] >= from) {
+            labels_o.push(labels_e[i])
+        }
+    }
+
+    if (type_e == 'mw') {
+        var ctx_salary_mw = document.getElementById('salary_mw');
+        var myChart = new Chart(ctx_salary_mw, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: [{
+                        label: 'Woman',
+                        data: data_woman,
+                        borderColor: "blue",
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Man',
+                        data: data_man,
+                        borderColor: 'red',
+                        borderWidth: 1
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+    }
+    if (type_e == 'wh') {
+        var salary_mw = document.getElementById('salary_mw');
+        var myChart = new Chart(salary_mw, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: [{
+                    label: 'Whole',
+                    data: data_e,
+                    borderColor: 'red',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+
+    }
+
+    if (type_e == 'parts') {
+        datasets_m = []
+        colors = ['#B80000', '#B8A800', '#B8FF00', '#B8FF0B', '#B8FFDF', '#B8FF43', '#B8FFEB', '#B839EB', '#0039EB', '#003907', '#594C21', '#522A1A', '#542A35', '#FBF28A', '#009D83', '#BD6D61']
+
+        for (var i = 0; i <= parts.length; i++) {
+            if (parts[i] != undefined) {
+                datasets_m.push(
+
+                    {
+                        label: parts[i],
+                        data: data_e[i],
+                        borderColor: colors[i],
+                        borderWidth: 1
+                    }
+
+                )
+            }
+
+        }
+
+
+        var ctx_salary_mw = document.getElementById('salary_mw');
+        var myChart = new Chart(ctx_salary_mw, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: datasets_m,
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+
+
+    }
+
+}
+
+
+
+//!CARS
+function cars(type_e, data_e, labels_e, data_man, data_woman, from, to, parts) {
+    var labels_o = []
+    data_o = []
+    data_man_o = []
+    data_woman_o = []
+
+    for (var i = 0; i <= labels_e.length; i++) {
+        if (labels_e[i] <= to & labels_e[i] >= from) {
+            labels_o.push(labels_e[i])
+        }
+    }
+
+    if (type_e == 'mw') {
+        var ctx_cars = document.getElementById('cars');
+        var myChart = new Chart(ctx_cars, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: [{
+                        label: 'Woman',
+                        data: data_woman,
+                        borderColor: "blue",
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Man',
+                        data: data_man,
+                        borderColor: 'red',
+                        borderWidth: 1
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+    }
+    if (type_e == 'wh') {
+        var ctx_cars = document.getElementById('cars');
+        var myChart = new Chart(ctx_cars, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: [{
+                    label: 'Whole',
+                    data: data_e,
+                    borderColor: 'red',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+    }
+
+    if (type_e == 'parts') {
+        datasets_m = []
+        colors = ['#B80000', '#B8A800', '#B8FF00', '#B8FF0B', '#B8FFDF', '#B8FF43', '#B8FFEB', '#B839EB', '#0039EB', '#003907', '#594C21', '#522A1A', '#542A35', '#FBF28A', '#009D83', '#BD6D61']
+
+        for (var i = 0; i <= parts.length; i++) {
+            if (parts[i] != undefined) {
+                datasets_m.push(
+
+                    {
+                        label: parts[i],
+                        data: data_e[i],
+                        borderColor: colors[i],
+                        borderWidth: 1
+                    }
+
+                )
+            }
+
+        }
+
+
+        var ctx_cars = document.getElementById('cars');
+        var myChart = new Chart(ctx_cars, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: datasets_m,
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+
+
+    }
+}
+
+
+
+
+//!SQUARE
+
+function flat(type_e, data_e, labels_e, data_man, data_woman, from, to, parts) {
+    var labels_o = []
+    data_o = []
+    data_man_o = []
+    data_woman_o = []
+
+    for (var i = 0; i <= labels_e.length; i++) {
+        if (labels_e[i] <= to & labels_e[i] >= from) {
+            labels_o.push(labels_e[i])
+        }
+    }
+
+    if (type_e == 'mw') {
+        var ctx_square = document.getElementById('square');
+        var myChart = new Chart(ctx_square, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: [{
+                        label: 'Woman',
+                        data: data_woman,
+                        borderColor: "blue",
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Man',
+                        data: data_man,
+                        borderColor: 'red',
+                        borderWidth: 1
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+    }
+    if (type_e == 'wh') {
+        var ctx_square = document.getElementById('square');
+        var myChart = new Chart(ctx_square, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: [{
+                    label: 'Whole',
+                    data: data_e,
+                    borderColor: 'red',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+    }
+
+    if (type_e == 'parts') {
+        datasets_m = []
+        colors = ['#B80000', '#B8A800', '#B8FF00', '#B8FF0B', '#B8FFDF', '#B8FF43', '#B8FFEB', '#B839EB', '#0039EB', '#003907', '#594C21', '#522A1A', '#542A35', '#FBF28A', '#009D83', '#BD6D61']
+
+        for (var i = 0; i <= parts.length; i++) {
+            if (parts[i] != undefined) {
+                datasets_m.push(
+
+                    {
+                        label: parts[i],
+                        data: data_e[i],
+                        borderColor: colors[i],
+                        borderWidth: 1
+                    }
+
+                )
+            }
+
+        }
+
+
+        var ctx_square = document.getElementById('square');
+        var myChart = new Chart(ctx_square, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: datasets_m,
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+
+
+    }
+}
+
+
+//savings
+
+function savings(type_e, data_e, labels_e, data_man, data_woman, from, to, parts) {
+    var labels_o = []
+    data_o = []
+    data_man_o = []
+    data_woman_o = []
+
+    for (var i = 0; i <= labels_e.length; i++) {
+        if (labels_e[i] <= to & labels_e[i] >= from) {
+            labels_o.push(labels_e[i])
+        }
+    }
+
+    if (type_e == 'mw') {
+        var ctx_savings = document.getElementById('savings');
+        var myChart = new Chart(ctx_savings, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: [{
+                        label: 'Woman',
+                        data: data_woman,
+                        borderColor: "blue",
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Man',
+                        data: data_man,
+                        borderColor: 'red',
+                        borderWidth: 1
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                },
+                legend: {
+                    onClick: (e) => null
+                }
+            }
+        });
+    }
+    if (type_e == 'wh') {
+        var ctx_savings = document.getElementById('savings');
+        var myChart = new Chart(ctx_savings, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: [{
+                    label: 'Whole',
+                    data: data_e,
+                    borderColor: 'red',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+    }
+
+    if (type_e == 'parts') {
+        datasets_m = []
+        colors = ['#B80000', '#B8A800', '#B8FF00', '#B8FF0B', '#B8FFDF', '#B8FF43', '#B8FFEB', '#B839EB', '#0039EB', '#003907', '#594C21', '#522A1A', '#542A35', '#FBF28A', '#009D83', '#BD6D61']
+
+        for (var i = 0; i <= parts.length; i++) {
+            if (parts[i] != undefined) {
+                datasets_m.push(
+
+                    {
+                        label: parts[i],
+                        data: data_e[i],
+                        borderColor: colors[i],
+                        borderWidth: 1
+                    }
+
+                )
+            }
+
+        }
+
+
+        var ctx_savings = document.getElementById('savings');
+        var myChart = new Chart(ctx_savings, {
+            type: 'line',
+            data: {
+                labels: labels_o,
+                datasets: datasets_m,
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                }
+            }
+        });
+
+
+    }
+}
