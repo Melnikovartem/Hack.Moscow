28f664006bfbb788f6ccb2e8119b1f75ecf9ee94
Dmitriy Shabat <mitya.shabat@gmail.com>
2019-10-27 00:37:32 +0300

    Fixed csv & code

--- backend/office_scraping.py
+++ backend/office_scraping.py
@@ -4,20 +4,20 @@ import pickle
 
 def make_region_lists():
     region_to_offices = {}
-    data = pd.read_csv('offices2.csv', sep=';').set_index('id')
-    for id, region in data['region_id']:
-        if type(region) == str:
+    data = pd.read_csv('offices_reg.csv', sep=';').set_index('id')
+    for id, region in data['region_id'].items():
+        if not np.isnan(region):
             id, region = int(id), int(region)
             if region not in region_to_offices:
                 region_to_offices[region] = []
             region_to_offices[region].append(id)
     return region_to_offices
 def tree_unfold():
-    tree, parent = pickle.load(open('tree_parent', 'rb'))
+    tree, parent = pickle.load(open('tree_parent.dump', 'rb'))
     unfold = {}
     def recursive_children(node):
         if node not in tree:
-            return
+            return []
         res = []
         for innode in tree[node]:
             res.append(innode)
@@ -31,3 +31,6 @@ def tree_unfold():
             continue
         unfold[root] = recursive_children(root)
     return unfold
+
+if __name__ == '__main__':
+    print(make_region_lists().keys())
