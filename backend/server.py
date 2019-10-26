47504c7f3a49d832f652ac5da746da973b1010e3
Your Name <you@example.com>
2019-10-26 22:35:11 +0300

    Back

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from parser import office_data
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def main():
@@ -20,8 +20,8 @@ def result(office_id):
     print(office.party_avg("incomes")) # по партиям средняя зарплата
     # Пример: (['Нет Данных', 'Единая Россия'], [[None, None, None, None, None, None, None, 2769537.14, 1567386.81, 1478636.39, 1445398.08], [975657.0, 1063445.0, 1215654.67, 1397083.65, 1769658.0, 2213239.15, 3031778.9, None, None, None, None]])
 
-    # Денис скоро добавит остальные функции 
+    # Денис скоро добавит остальные функции
     return render_template("result.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
